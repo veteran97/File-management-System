c378adadafed6d35806740dd0ab74345c63148bc
Shashank Kumar <33193319+veteran97@users.noreply.github.com>
2019-06-26 13:12:35 +0530

    Add files via upload

--- file.js
+++ file.js
@@ -1,15 +1,132 @@
-var mongoose=require('mongoose');
-var autoIncrement = require('mongoose-sequence')(mongoose);
+const express = require('express');
+const router = express.Router();
 
-var FileSchema=mongoose.Schema({
-  name : {type:String },
-  type :{type:String},
-  createdOn:{type:Number },
-  createdBy:{type:String},
-  details:{type:String},
-  movement:[{from:String,to:String,movedOn:Number,remark:String}]
+const File = require('../models/file');
+const Department = require('../models/department');
+
+router.post('/add',ensureAuthenticated,(req,res)=>{
+	//console.log(req.body);
+	var newFile = File();
+	newFile.name = req.body.name;
+	newFile.type = req.body.type;
+	newFile.details = req.body.details;
+	newFile.createdBy = req.body.createdBy;
+	newFile.createdOn = new Date(req.body.createdOn).getTime();
+	newFile.save();
+	res.redirect('/user/home');
+})
+
+router.get('/delete/:fileId',ensureAuthenticated,(req,res)=>{
+	File.remove({_id:req.params.fileId},(err)=>{
+		if(err)
+      		throw err;
+    	req.flash('success_msg', 'File Succesfully Deleted');
+    	res.redirect('/user/home');
+	});
+})
+
+router.get('/edit/:fileId',ensureAuthenticated,(req,res)=>{
+	File.findOne({_id:req.params.fileId},(err,file)=>{
+		if(err) throw err;
+		Department.find({},(err,departments)=>{
+			if(err) throw err;
+			res.render('file',{movement:file.movement,file:file,departments:departments});
+		})
+	});
+})
+
+router.post('/update/:fileId',ensureAuthenticated,(req,res)=>{
+	//console.log(req.body);
+	const movedon = new Date(req.body.movedOn).getTime();
+	//console.log(movedon);
+	File.findOneAndUpdate(
+		{_id:req.params.fileId},
+		{$push:{movement:{from:req.body.from,to:req.body.to,movedOn:movedon,remark:req.body.remark}}},
+		(err,file)=>{
+			if(err)
+				throw(err);
+			else
+				res.redirect(`/file/edit/${req.params.fileId}`)
+		});
+})
+
+router.get('/delete/:fileId/:moveId',ensureAuthenticated,(req,res)=>{
+	File.findOne({_id:req.params.fileId},(err,file)=>{
+		if(err) throw err;
+		File.update({_id:req.params.fileId},
+								{$pull:{movement:{_id:req.params.moveId}}},
+								(err,file)=>{
+									if(err) throw err;
+									res.redirect(`/file/edit/${req.params.fileId}`);
+								});
+	})
 });
 
-FileSchema.plugin(autoIncrement, {inc_field: 'fileId'});
+router.get('/report1',ensureAuthenticated,(req,res)=>{
+	res.render('report1',{movement:[]});
+});
 
-var File = module.exports = mongoose.model('File', FileSchema);
+router.post('/report1',ensureAuthenticated,(req,res)=>{
+	var from = new Date(req.body.from).getTime();
+	var to = new Date(req.body.to).getTime();
+	File.find({},(err,files)=>{
+		if(err) throw err;
+		var arr = [];
+		for(var i=0;i<files.length;i++)
+		{
+			//console.log(files[i]._id);
+			for(var j=0;j<files[i].movement.length;j++)
+			{
+					var ti = files[i].movement[j].movedOn;
+					if(ti>=from && ti <=to)
+					{
+						//console.log(ti);
+						arr.push({
+							fileId:files[i].fileId,
+							from:files[i].movement[j].from,
+							to:files[i].movement[j].to,
+							movedOn:files[i].movement[j].movedOn,
+							remark:files[i].movement[j].remark
+						});
+					}
+			}
+		}
+		res.render('report11',{movement:arr,from:from,to:to});
+	});
+})
+
+router.get('/report2',ensureAuthenticated,(req,res)=>{
+	Department.find({},(err,departments)=>{
+		if(err) throw err;
+		res.render('report2',{files:[],departments:departments});
+	})
+})
+
+router.post('/report2',ensureAuthenticated,(req,res)=>{
+	//console.log(req.body);
+	File.find({"createdBy":req.body.department},(err,files)=>{
+		if(err) throw err;
+		Department.find({},(err,departments)=>{
+			if(err) throw err;
+			res.render('report22',{files:files,department:req.body.department});
+		})
+	});
+})
+
+router.get('/report3',ensureAuthenticated,(req,res)=>{
+	File.find({},(err,files)=>{
+		if(err) throw err;
+		res.render('report3',{files:files});
+	})
+})
+
+function ensureAuthenticated(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  } else {
+    req.flash('danger', 'Please login');
+    res.redirect('/user/login');
+  }
+}
+
+module.exports = router;
--- user.js
+++ user.js
@@ -1,9 +1,141 @@
-var mongoose=require('mongoose');
+const express = require('express');
+const bcrypt = require('bcryptjs');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 
-var UserSchema=mongoose.Schema({
-  email:{ type:String },
-  username:{  type:String },
-  password:{  type:String }
+const router = express.Router();
+
+const User = require('../models/user');
+const Department = require('../models/department');
+const Filetype = require('../models/filetype');
+	
+router.get('/login',(req,res)=>{
+	res.render('login');
+})
+
+passport.use(new LocalStrategy(function(username, password, done){
+    var query = {email:username};
+    User.findOne(query,(err, user)=>{
+      if(err) throw err;
+      if(!user){
+        return done(null, false, {message: 'No user found'});
+      }
+      // console.log(password);
+      // console.log(user.password);
+      bcrypt.compare(password, user.password,(err, isMatch)=>{
+        if(err) throw err;
+        if(isMatch){
+          return done(null, user);
+        } else {
+          return done(null, false, {message: 'Wrong password'});
+        }
+      });
+    });
+  }));
+
+passport.serializeUser(function(user, done) {
+	done(null, user.id);
 });
 
-var User= module.exports = mongoose.model('User', UserSchema);
+passport.deserializeUser(function(id, done) {
+	User.findById(id, function(err, user) {
+	  done(err, user);
+	});
+});
+
+router.post('/login',
+	passport.authenticate('local',{successRedirect:'/user/home',failureRedirect:'/user/login',failureFlash:true}),
+	(req,res)=>{
+		res.redirect('/user/home')
+	})
+
+router.get('/signup',(req,res)=>{
+	res.render('signup');
+})
+
+router.post('/signup',(req,res)=>{
+	if(req.body.secretKey.toString() == 'guru')
+	{
+		//console.log("match");
+		var newUser = User();
+		newUser.email = req.body.email
+		newUser.username = req.body.username;
+		bcrypt.genSalt(10,(err,salt)=>{
+			if(err) throw err;
+			bcrypt.hash(req.body.password,salt,(err,hash)=>{
+				if(err) throw err;
+				newUser.password = hash;
+				newUser.save();
+				res.redirect('/user/login');
+			});
+		})
+	}
+	else
+	{
+		//console.log("does not match");
+		res.redirect('/user/signup');
+
+	}
+
+})
+
+router.get('/logout',(req,res)=>{
+	req.logout();
+	res.redirect('/');
+})
+
+router.get('/add/department',ensureAuthenticated,(req,res)=>{
+	Department.find({},(err,departments)=>{
+		if(err) throw err;
+		res.render('department',{departments:departments});
+	})
+})	
+
+router.post('/add/department',ensureAuthenticated,(req,res)=>{
+	var newDepartment = Department();
+	newDepartment.name = req.body.name;
+	newDepartment.save();
+	res.redirect('/user/add/department');
+})
+
+router.get('/delete/department/:departmentId',ensureAuthenticated,(req,res)=>{
+	Department.remove({_id:req.params.departmentId},(err,department)=>{
+		if(err) throw err;
+    	req.flash('success_msg', 'Department Succesfully Deleted');
+    	res.redirect('/user/add/department');
+	})
+})
+
+router.get('/add/filetype',ensureAuthenticated,(req,res)=>{
+	Filetype.find({},(err,filetypes)=>{
+		if(err) throw err;
+		res.render('filetype',{types:filetypes});
+	})
+})
+
+router.post('/add/filetype',ensureAuthenticated,(req,res)=>{
+	var newFiletype = Filetype();
+	newFiletype.name = req.body.name;
+	newFiletype.save();
+	res.redirect('/user/add/filetype');
+})
+
+router.get('/delete/filetype/:filetypeId',ensureAuthenticated,(req,res)=>{
+	Filetype.remove({_id:req.params.filetypeId},(err,filetype)=>{
+		if(err) throw err;
+    	req.flash('success_msg', 'Filetype Succesfully Deleted');
+    	res.redirect('/user/add/filetype');
+	})
+})
+
+function ensureAuthenticated(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  } else {
+    req.flash('danger', 'Please login');
+    res.redirect('/user/login');
+  }
+}
+
+module.exports = router;
+
